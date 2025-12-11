7a44b67522e8f398500ed4536200e169b950c367
Anthony <tonyxiz10@gmail.com>
2025-12-11 12:26:58 -0500

    commit

--- EjerciciosT9/src/paquete01/Ejercicio.java
+++ EjerciciosT9/src/paquete01/Ejercicio.java
@@ -4,10 +4,51 @@
  */
 package paquete01;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Ejercicio {
-    
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        String dato;
+        String letra;
+        int cantidad;
+        int i = 0;
+        System.out.println("¿Cuantas marcas desea ingresar?");
+        cantidad = entrada.nextInt();
+        entrada.nextLine();
+
+        String[] arreglo1 = new String[cantidad];
+
+        while (i < arreglo1.length) {
+            System.out.println("Ingrese una marca de vehiculo (no se permite  "
+                    + "que la marca empiece con estas letras A, C, T)");
+            dato = entrada.nextLine();
+            letra = dato.substring(0, 1);
+            letra = letra.toLowerCase();
+            switch (letra) {
+                case "a":
+                case "c":
+                case "t":
+                    System.out.println("Marca invalida");
+                    break;
+                default:
+                    arreglo1[i] = dato;
+                    i++;
+            }
+        }
+        System.out.println("\nLista de marcas ingresadas\n"
+                + "--------------------------------------");
+        for (int e = 0; e < arreglo1.length; e++) {
+            System.out.printf("Marca %d: %s\n",
+                    e, arreglo1[e]);
+        }
+
+    }
 }
