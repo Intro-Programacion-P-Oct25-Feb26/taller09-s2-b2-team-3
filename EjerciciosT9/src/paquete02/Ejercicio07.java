352edb2aa010f4db3382e7ecec10e86e42fa4b85
svalverde007 <servio.valverde.uemv@gmail.com>
2025-12-11 20:11:25 -0500

    ejercicio 07

--- EjerciciosT9/src/paquete02/Ejercicio07.java
+++ EjerciciosT9/src/paquete02/Ejercicio07.java
@@ -8,8 +8,9 @@ package paquete02;
 import java.util.Scanner;
 
 /**
- *
- * @author reroes
+ * Diseñe una solución que permita al usuario introducir 10 números en un
+ * arreglo, luego despliegue cada número y su diferencia a partir del promedio
+ * numérico de los números introducidos.
  */
 public class Ejercicio07 {
 
@@ -17,8 +18,32 @@ public class Ejercicio07 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      
+        Scanner entrada = new Scanner(System.in);
 
+        int suma = 0;
+        double promedio;
+        int[] numeros = new int[10];
+
+        System.out.println("Ingrese 10 números:");
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Número " + (i + 1) + ":");
+            numeros[i] = entrada.nextInt();
+            suma += numeros[i];
+        }
+
+        promedio = (double) suma / numeros.length;
+        System.out.println("\n\t\t\t =====RESULTADOS=====\n");
+
+        for (int i = 0; i < numeros.length; i++) {
+            double diferencia = numeros[i] - promedio;
+
+            System.out.printf("Número %d: %d ==> Promedio: %.2f "
+                    + "==> Diferencia con el promedio: %.2f\n",
+                    (i + 1),
+                    numeros[i],
+                    promedio,
+                    diferencia);
+        }
     }
-
 }
